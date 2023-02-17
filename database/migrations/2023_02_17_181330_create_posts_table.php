670d9cc7f983c60d5f53ba1b3b6328511851dbc8
Bektur211 <bekamaasaliev@gmail.com>
2023-02-18 00:34:59 +0600

    make migrations

---
+++ database/migrations/2023_02_17_181330_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table-> string("title");
+            $table-> text("content");
+            $table-> unsignedBigInteger("category_id")->nullable();
+            $table->timestamps();
+
+            $table->index("category_id","post_category_idx");
+            $table->foreign("category_id","post_category_fk")->on("categories")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
