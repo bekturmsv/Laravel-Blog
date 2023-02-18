3d009d09f16ac1267e6a34193be722ffa3f94012
Bektur211 <bekamaasaliev@gmail.com>
2023-02-18 18:25:45 +0600

    add new route for category

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::group(["namespace" => "App\Http\Controllers\Admin", "prefix" => "admin"],
     Route::group(["namespace"=> "Main"], function(){
         Route::get("/","IndexController");
     });
+    
+    Route::group(["namespace"=> "Category", "prefix"=>"categories"], function(){
+        Route::get("/","IndexController");
+    });
 });
 
 
