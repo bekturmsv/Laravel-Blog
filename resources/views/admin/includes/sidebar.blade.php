20d3e53d79020ead4d04a6ee83390bceec04ce98
Bektur211 <bekamaasaliev@gmail.com>
2023-02-18 18:24:44 +0600

    add include sidebar

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,17 @@
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview">
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-th-list"></i>
+            <p>
+              Категория
+            </p>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- /.sidebar -->
+  </aside>
