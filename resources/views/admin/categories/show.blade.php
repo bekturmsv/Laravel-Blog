aa56fc79d4720ff6e7682a7f4753d56c7ac181f1
Bektur211 <bekamaasaliev@gmail.com>
2023-02-19 00:24:55 +0600

    add new view for show controller

---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,71 @@
+ @extends("admin.layouts.main")
+ @section("content")
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">{{$category->title}}</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Категория</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+
+          <div class="row">
+          <div class="col-8">
+
+              <div class="card">
+                  <div class="card-body table-responsive p-0">
+                      <table class="table table-hover text-nowrap">
+
+                          <tbody>
+                          <tr>
+                              <td>ID</td>
+                              <td>{{$category -> id}}}</td>
+                              <td><a href=""><i class="far fa-eye"></i></a></td>
+                          </tr>
+                          <tr>
+                              <td>Название</td>
+                              <td>{{$category -> title}}}</td>
+                              <td><a href=""><i class="far fa-eye"></i></a></td>
+                          </tr>
+                          </tbody>
+                      </table>
+                  </div>
+                  <!-- /.card-body -->
+              </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Blog on Laravel.</strong>
+
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+@endsection
