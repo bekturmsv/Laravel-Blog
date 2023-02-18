61a7ad64a942400eb69823d928e95633d23c22c3
Bektur211 <bekamaasaliev@gmail.com>
2023-02-18 21:39:16 +0600

    create request for fetching all categories

--- app/Http/Controllers/Admin/Category/IndexController.php
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class IndexController extends Controller
 {
      function __invoke()
     {
-        return view("admin.categories.index");
+        $categories = Category::all();
+        return view("admin.categories.index", compact("categories"));
     }
 }
