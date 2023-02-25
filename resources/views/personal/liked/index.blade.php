894a7275e875a66ee526e10057254af4c580d189
Bektur211 <bekamaasaliev@gmail.com>
2023-02-25 23:42:16 +0600

    added new page for liked posts

--- resources/views/personal/liked/index.blade.php
+++ resources/views/personal/liked/index.blade.php
@@ -21,6 +21,45 @@
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
+            <div class="col-8">
+
+                <div class="card">
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap text-center">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Название</th>
+                                <th colspan="2">Действие</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td>{{$post-> id}}</td>
+                                    <td>{{$post -> title}}</td>
+                                    <td><a href="{{route('admin.post.show', $post->id)}}" ><i
+                                                class="far fa-eye"></i></a></td>
+
+                                    <td>
+                                        <form action="{{route('personal.liked.delete', $post->id)}}" method="POST">
+                                            @csrf
+                                            @method("DELETE")
+                                            <button type="submit" class="border-0 bg-transparent">
+                                                <i class="fas fa-trash text-danger" role="button"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
 
         </div>
         <!-- /.row -->
