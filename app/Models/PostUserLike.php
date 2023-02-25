618826ac4f9195a5cdf8adb63e45f382572f49e7
Bektur211 <bekamaasaliev@gmail.com>
2023-02-25 23:41:21 +0600

    adding new table post_user_likes for the connection of many to many

---
+++ app/Models/PostUserLike.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostUserLike extends Model
+{
+    use HasFactory;
+    protected $table = "post_user_likes";
+    protected $guarded = false;
+}
